550f06f8c774a97979a7618d86f8e3a653fc810a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-22 13:16:05 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisionComputation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to line comparison computation");//master
+        }
+    }
+}
